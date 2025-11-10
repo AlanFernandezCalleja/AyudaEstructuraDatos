28e0fff089ad9f7dbc30c04991cdc9b85477d216
mauri <alan.fernandez@ucb.edu.bo>
2025-11-10 11:32:29 -0400

    subiendo ejemplo de arboles

---
+++ Arboles/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        run("Programa de Arboles Binarios");
+
+        
+    }
+
+
+
+
+
+    public static void run(String tittle){
+        System.out.println("===============================================");
+        System.out.println("\n\n\t"+(tittle)+"\n\n");
+        System.out.println("===============================================\n");
+    } 
+}
---
+++ Arboles/Nodo.java
@@ -0,0 +1,10 @@
+public class Nodo {
+    public int value;
+    public Nodo left;
+    public Nodo rght;
+
+    public Nodo(int value){
+        this.value = value;
+    }
+
+}
